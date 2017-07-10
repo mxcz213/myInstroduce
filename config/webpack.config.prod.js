4678a0645b5303026f012bf67a2bcac79b52a9b0
chenjuanhe <chenjuanhe@pptv.com>
2017-07-10 11:16:56 +0800

    build

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -24,7 +24,7 @@ const shouldUseRelativeAssetPaths = publicPath === './';
 // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
 const publicUrl = publicPath.slice(0, -1);
 // Get environment variables to inject into our app.
-const env = getClientEnvironment(publicUrl);
+const env = getClientEnvironment(__dirname + '/build/');
 
 // Assert this just to be safe.
 // Development builds of React are slow and not intended for production.
